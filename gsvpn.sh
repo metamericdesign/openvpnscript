46b9e09d50262bbbf5de0be2fc334edd1d71020a
mick-wloch <54990772+mick-wloch@users.noreply.github.com>
2023-01-05 14:51:18 -0600

    handle auto basesta generation

--- gsvpn.sh
+++ gsvpn.sh
@@ -107,115 +107,7 @@ function initialCheck() {
 }
 
 function installUnbound() {
-	# If Unbound isn't installed, install it
-	if [[ ! -e /etc/unbound/unbound.conf ]]; then
-
-		if [[ $OS =~ (debian|ubuntu) ]]; then
-			apt-get install -y unbound
-
-			# Configuration
-			echo 'interface: 10.8.0.1
-access-control: 10.8.0.1/24 allow
-hide-identity: yes
-hide-version: yes
-use-caps-for-id: yes
-prefetch: yes' >>/etc/unbound/unbound.conf
-
-		elif [[ $OS =~ (centos|amzn|oracle) ]]; then
-			yum install -y unbound
-
-			# Configuration
-			sed -i 's|# interface: 0.0.0.0$|interface: 10.8.0.1|' /etc/unbound/unbound.conf
-			sed -i 's|# access-control: 127.0.0.0/8 allow|access-control: 10.8.0.1/24 allow|' /etc/unbound/unbound.conf
-			sed -i 's|# hide-identity: no|hide-identity: yes|' /etc/unbound/unbound.conf
-			sed -i 's|# hide-version: no|hide-version: yes|' /etc/unbound/unbound.conf
-			sed -i 's|use-caps-for-id: no|use-caps-for-id: yes|' /etc/unbound/unbound.conf
-
-		elif [[ $OS == "fedora" ]]; then
-			dnf install -y unbound
-
-			# Configuration
-			sed -i 's|# interface: 0.0.0.0$|interface: 10.8.0.1|' /etc/unbound/unbound.conf
-			sed -i 's|# access-control: 127.0.0.0/8 allow|access-control: 10.8.0.1/24 allow|' /etc/unbound/unbound.conf
-			sed -i 's|# hide-identity: no|hide-identity: yes|' /etc/unbound/unbound.conf
-			sed -i 's|# hide-version: no|hide-version: yes|' /etc/unbound/unbound.conf
-			sed -i 's|# use-caps-for-id: no|use-caps-for-id: yes|' /etc/unbound/unbound.conf
-
-		elif [[ $OS == "arch" ]]; then
-			pacman -Syu --noconfirm unbound
-
-			# Get root servers list
-			curl -o /etc/unbound/root.hints https://www.internic.net/domain/named.cache
-
-			if [[ ! -f /etc/unbound/unbound.conf.old ]]; then
-				mv /etc/unbound/unbound.conf /etc/unbound/unbound.conf.old
-			fi
-
-			echo 'server:
-	use-syslog: yes
-	do-daemonize: no
-	username: "unbound"
-	directory: "/etc/unbound"
-	trust-anchor-file: trusted-key.key
-	root-hints: root.hints
-	interface: 10.8.0.1
-	access-control: 10.8.0.1/24 allow
-	port: 53
-	num-threads: 2
-	use-caps-for-id: yes
-	harden-glue: yes
-	hide-identity: yes
-	hide-version: yes
-	qname-minimisation: yes
-	prefetch: yes' >/etc/unbound/unbound.conf
-		fi
-
-		# IPv6 DNS for all OS
-		if [[ $IPV6_SUPPORT == 'y' ]]; then
-			echo 'interface: fd42:42:42:42::1
-access-control: fd42:42:42:42::/112 allow' >>/etc/unbound/unbound.conf
-		fi
-
-		if [[ ! $OS =~ (fedora|centos|amzn|oracle) ]]; then
-			# DNS Rebinding fix
-			echo "private-address: 10.0.0.0/8
-private-address: fd42:42:42:42::/112
-private-address: 172.16.0.0/12
-private-address: 192.168.0.0/16
-private-address: 169.254.0.0/16
-private-address: fd00::/8
-private-address: fe80::/10
-private-address: 127.0.0.0/8
-private-address: ::ffff:0:0/96" >>/etc/unbound/unbound.conf
-		fi
-	else # Unbound is already installed
-		echo 'include: /etc/unbound/openvpn.conf' >>/etc/unbound/unbound.conf
-
-		# Add Unbound 'server' for the OpenVPN subnet
-		echo 'server:
-interface: 10.8.0.1
-access-control: 10.8.0.1/24 allow
-hide-identity: yes
-hide-version: yes
-use-caps-for-id: yes
-prefetch: yes
-private-address: 10.0.0.0/8
-private-address: fd42:42:42:42::/112
-private-address: 172.16.0.0/12
-private-address: 192.168.0.0/16
-private-address: 169.254.0.0/16
-private-address: fd00::/8
-private-address: fe80::/10
-private-address: 127.0.0.0/8
-private-address: ::ffff:0:0/96' >/etc/unbound/openvpn.conf
-		if [[ $IPV6_SUPPORT == 'y' ]]; then
-			echo 'interface: fd42:42:42:42::1
-access-control: fd42:42:42:42::/112 allow' >>/etc/unbound/openvpn.conf
-		fi
-	fi
-
-	systemctl enable unbound
-	systemctl restart unbound
+	echo "removed"
 }
 
 function installQuestions() {
@@ -1024,6 +916,22 @@ verb 3" >>/etc/openvpn/client-template.txt
 }
 
 function newClient() {
+	
+	#check for autogeneration params
+	AUTOCLIENTTYPE=$1
+	AUTOCLIENTNAME=$2
+	AUTONETNUM=$3
+	if [[ $AUTOCLIENTTYPE ]]; then
+
+	echo "type: $AUTOCLIENTTYPE";
+	echo "client: $AUTOCLIENTNAME";
+	echo "prod net num: $AUTONETNUM";
+	echo "New auto client..."
+	
+	fi
+	
+	
+	
 	# determine cloud network to route
 
 	if [[ ! -e /etc/orgnetnum ]]; then
@@ -1040,6 +948,7 @@ function newClient() {
 	echo "   1) Basestation"
 	echo "   2) Desktop (default)"
 
+	PASS="$AUTOCLIENTTYPE"
 	until [[ $PASS =~ ^[1-2]$ ]]; do
 		read -rp "Select an option [1-2]: " -e PASS
 	done
@@ -1048,6 +957,7 @@ function newClient() {
 	echo "Tell me a name for the client."
 	echo "The name must consist of alphanumeric character. It may also include an underscore or a dash."
 
+	CLIENT="$AUTOCLIENTNAME"
 	until [[ $CLIENT =~ ^[a-zA-Z0-9_-]+$ ]]; do
 		read -rp "Client name: " -e CLIENT
 	done
@@ -1108,7 +1018,7 @@ function newClient() {
 		echo ""
 		echo "Select Production Network to connect. 172.16.X.0"
 		echo "Default = 1 (172.16.1.0)"
-	
+		OCTET3="$AUTONETNUM"
 		until [[ $OCTET3 =~ ^[1-9]$|^[1-9][0-9]$|^(254)$ ]]; do
 			read -rp "Type the third octet of the production LAN [1-254]: " -e OCTET3
 		done
@@ -1372,9 +1282,18 @@ function manageMenu() {
 }
 
 
-echo "type: $1";
-echo "client: $2";
-echo "netnum: $3";
+AUTOCLIENTTYPE=$1
+AUTOCLIENTNAME=$2
+AUTONETNUM=$3
+if [[ $AUTOCLIENTTYPE ]]; then
+
+# echo "type: $AUTOCLIENTTYPE";
+# echo "client: $AUTOCLIENTNAME";
+# echo "net num: $AUTONETNUM";
+echo "Starting auto client generation."
+newClient "$AUTOCLIENTTYPE" "$AUTOCLIENTNAME" "$AUTONETNUM"
+fi
+
 # Check for root, TUN, OS...
 initialCheck
 
